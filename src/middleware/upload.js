8c2bc43b63e04dea3e0166ac05edd600f049c3f4
misbahx <misbahx.id@gmail.com>
2025-03-11 08:38:53 +0700

    feat: add validation upload directory

--- src/middleware/upload.js
+++ src/middleware/upload.js
@@ -1,5 +1,12 @@
 const multer = require("multer");
 const path = require("path");
+const fs = require("fs");
+
+const uploadDir = "public/uploads/";
+
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir);
+}
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
